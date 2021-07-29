1d987b317e0545ee0a4e9852f407c5ab1b00b0de
koga yusuke <popweb782@gmail.com>
2021-07-30 01:49:21 +0900

    add bootstrap

--- pf/app/public/wp-content/themes/minnanowordpress/functions.php
+++ pf/app/public/wp-content/themes/minnanowordpress/functions.php
@@ -140,10 +140,13 @@ add_action( 'widgets_init', 'minnanowordpress_widgets_init' );
  * Enqueue scripts and styles.
  */
 function minnanowordpress_scripts() {
-	wp_enqueue_style( 'minnanowordpress-style', get_stylesheet_uri(), array(), _S_VERSION );
+  wp_enqueue_style( 'bootstrap-style', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", array(), _S_VERSION );
+	wp_enqueue_style( 'minnanowordpress-style', get_stylesheet_uri(), array('bootstrap-style'), _S_VERSION );
 	wp_style_add_data( 'minnanowordpress-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'minnanowordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+  wp_enqueue_script( 'bootstrap-script', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js", array(), _S_VERSION );
+	wp_enqueue_script( 'minnanowordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array('bootstrap-script'), _S_VERSION, true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
