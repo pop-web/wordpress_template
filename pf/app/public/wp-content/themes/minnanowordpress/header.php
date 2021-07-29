4d600735120b2a011513ce79f4dd01aa6bdc6c16
koga yusuke <popweb782@gmail.com>
2021-07-30 01:49:37 +0900

    no message

--- pf/app/public/wp-content/themes/minnanowordpress/header.php
+++ pf/app/public/wp-content/themes/minnanowordpress/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,48 +13,50 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'minnanowordpress' ); ?></a>
+  <?php wp_body_open(); ?>
+  <div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'minnanowordpress'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$minnanowordpress_description = get_bloginfo( 'description', 'display' );
-			if ( $minnanowordpress_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $minnanowordpress_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header">
+      <div class="site-branding">
+        <?php
+        the_custom_logo();
+        if (is_front_page() && is_home()) :
+        ?>
+          <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="text-decoration-none text-dark"><?php bloginfo('name'); ?></a></h1>
+        <?php
+        else :
+        ?>
+          <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="text-decoration-none text-dark"><?php bloginfo('name'); ?></a></p>
+        <?php
+        endif;
+        $minnanowordpress_description = get_bloginfo('description', 'display');
+        if ($minnanowordpress_description || is_customize_preview()) :
+        ?>
+          <p class="site-description"><?php echo $minnanowordpress_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+                                      ?></p>
+        <?php endif; ?>
+      </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'minnanowordpress' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+      <nav id="site-navigation" class="main-navigation">
+        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'minnanowordpress'); ?></button>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+          )
+        );
+        ?>
+      </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
