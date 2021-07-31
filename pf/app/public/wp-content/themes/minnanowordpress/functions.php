ab36ac8e3acab88338455832a9b7570b3f1e9313
koga yusuke <popweb782@gmail.com>
2021-07-31 11:52:59 +0900

    no message

--- pf/app/public/wp-content/themes/minnanowordpress/functions.php
+++ pf/app/public/wp-content/themes/minnanowordpress/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * minnanowordpress functions and definitions
  *
@@ -7,102 +8,103 @@
  * @package minnanowordpress
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+if (!defined('_S_VERSION')) {
+  // Replace the version number of the theme on each release.
+  define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'minnanowordpress_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function minnanowordpress_setup() {
-		/*
+if (!function_exists('minnanowordpress_setup')) :
+  /**
+   * Sets up theme defaults and registers support for various WordPress features.
+   *
+   * Note that this function is hooked into the after_setup_theme hook, which
+   * runs before the init hook. The init hook is too late for some features, such
+   * as indicating support for post thumbnails.
+   */
+  function minnanowordpress_setup()
+  {
+    /*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on minnanowordpress, use a find and replace
 		 * to change 'minnanowordpress' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'minnanowordpress', get_template_directory() . '/languages' );
+    load_theme_textdomain('minnanowordpress', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support('automatic-feed-links');
 
-		/*
+    /*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
-		/*
+    /*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'minnanowordpress' ),
-			)
-		);
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus(
+      array(
+        'menu-1' => esc_html__('Primary', 'minnanowordpress'),
+      )
+    );
 
-		/*
+    /*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+    add_theme_support(
+      'html5',
+      array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+        'style',
+        'script',
+      )
+    );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'minnanowordpress_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
+    // Set up the WordPress core custom background feature.
+    add_theme_support(
+      'custom-background',
+      apply_filters(
+        'minnanowordpress_custom_background_args',
+        array(
+          'default-color' => 'ffffff',
+          'default-image' => '',
+        )
+      )
+    );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+    // Add theme support for selective refresh for widgets.
+    add_theme_support('customize-selective-refresh-widgets');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
+    /**
+     * Add support for core custom logo.
+     *
+     * @link https://codex.wordpress.org/Theme_Logo
+     */
+    add_theme_support(
+      'custom-logo',
+      array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-width'  => true,
+        'flex-height' => true,
+      )
+    );
+  }
 endif;
-add_action( 'after_setup_theme', 'minnanowordpress_setup' );
+add_action('after_setup_theme', 'minnanowordpress_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,48 +113,50 @@ add_action( 'after_setup_theme', 'minnanowordpress_setup' );
  *
  * @global int $content_width
  */
-function minnanowordpress_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'minnanowordpress_content_width', 640 );
+function minnanowordpress_content_width()
+{
+  $GLOBALS['content_width'] = apply_filters('minnanowordpress_content_width', 640);
 }
-add_action( 'after_setup_theme', 'minnanowordpress_content_width', 0 );
+add_action('after_setup_theme', 'minnanowordpress_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function minnanowordpress_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'minnanowordpress' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'minnanowordpress' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+function minnanowordpress_widgets_init()
+{
+  register_sidebar(
+    array(
+      'name'          => esc_html__('Sidebar', 'minnanowordpress'),
+      'id'            => 'sidebar-1',
+      'description'   => esc_html__('Add widgets here.', 'minnanowordpress'),
+      'before_widget' => '<section id="%1$s" class="widget %2$s">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h2 class="widget-title">',
+      'after_title'   => '</h2>',
+    )
+  );
 }
-add_action( 'widgets_init', 'minnanowordpress_widgets_init' );
+add_action('widgets_init', 'minnanowordpress_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function minnanowordpress_scripts() {
-  wp_enqueue_style( 'bootstrap-style', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", array(), _S_VERSION );
-	wp_enqueue_style( 'minnanowordpress-style', get_stylesheet_uri(), array('bootstrap-style'), _S_VERSION );
-	wp_style_add_data( 'minnanowordpress-style', 'rtl', 'replace' );
+function minnanowordpress_scripts()
+{
+  wp_enqueue_style('bootstrap-style', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", array(), _S_VERSION);
+  wp_enqueue_style('minnanowordpress-style', get_stylesheet_uri(), array('bootstrap-style'), _S_VERSION);
+  wp_style_add_data('minnanowordpress-style', 'rtl', 'replace');
 
-  wp_enqueue_script( 'bootstrap-script', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js", array(), _S_VERSION );
-	wp_enqueue_script( 'minnanowordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array('bootstrap-script'), _S_VERSION, true );
+  wp_enqueue_script('bootstrap-script', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js", array(), _S_VERSION);
+  wp_enqueue_script('minnanowordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array('bootstrap-script'), _S_VERSION, true);
 
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+  if (is_singular() && comments_open() && get_option('thread_comments')) {
+    wp_enqueue_script('comment-reply');
+  }
 }
-add_action( 'wp_enqueue_scripts', 'minnanowordpress_scripts' );
+add_action('wp_enqueue_scripts', 'minnanowordpress_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -177,7 +181,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+  require get_template_directory() . '/inc/jetpack.php';
 }
-
