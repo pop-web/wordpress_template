40dabb9df2b4e018272a7bc900128a243404664a
koga yusuke <popweb782@gmail.com>
2021-08-02 00:13:05 +0900

    カスタム投稿出力設定

--- pf/app/public/wp-content/themes/minnanowordpress/front-page.php
+++ pf/app/public/wp-content/themes/minnanowordpress/front-page.php
@@ -9,8 +9,10 @@ get_header();
 
 <main id="primary" class="site-main">
   <div class="container pt-5">
+    <h2 class="rich_font text-center fs-1">
+      <span class="border-bottom mb-4 px-5 pb-3 d-block w-50  mx-auto">WordPressテーマ</span>
+    </h2>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-
       <?php
       $args = array(
         'post_type' => 'themelist',
@@ -21,15 +23,15 @@ get_header();
       ?>
         <?php while ($query->have_posts()) : $query->the_post(); ?>
           <div class="col">
-            <a href="" class="text-decoration-none text-dark">
+            <a href="<?php the_permalink(); ?>" class="text-decoration-none text-dark">
               <div class="card h-100">
                 <?php if (get_field('thumb')) : ?>
                   <img src="<?php the_field('thumb'); ?>" class="card-img-top" />
                 <?php endif; ?>
                 <div class="card-body">
-                  <h5 class="card-title d-flex justify-content-between fs-3 wf-sawarabimincho">
+                  <h5 class="card-title d-flex justify-content-between fs-3 rich_font">
                     <span><?php the_title(); ?></span>
-                    <span><?php echo "¥" . number_format((int) get_field('price')); ?></span>
+                    <span><?php echo "¥ " . number_format((int) get_field('price')); ?></span>
                   </h5>
                   <p class="card-text">
                     <?php the_field('note'); ?>
