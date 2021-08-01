d9a9b71190cedaebad01b9762589fdf3c8264d08
koga yusuke <popweb782@gmail.com>
2021-08-01 22:38:41 +0900

    add acf

--- pf/app/public/wp-content/themes/minnanowordpress/front-page.php
+++ pf/app/public/wp-content/themes/minnanowordpress/front-page.php
@@ -10,50 +10,37 @@ get_header();
 <main id="primary" class="site-main">
   <div class="container pt-5">
     <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <a href="" class="text-decoration-none text-dark">
-          <div class="card h-100">
-            <img src="https://unsplash.it/g/300/200?random" 　class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+
+      <?php
+      $args = array(
+        'post_type' => 'themelist',
+        'posts_per_page' => '-1'
+      );
+      $query = new WP_query($args);
+      if ($query->have_posts()) :
+      ?>
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
+          <div class="col">
+            <a href="" class="text-decoration-none text-dark">
+              <div class="card h-100">
+                <?php if (get_field('thumb')) : ?>
+                  <img src="<?php the_field('thumb'); ?>" class="card-img-top" />
+                <?php endif; ?>
+                <div class="card-body">
+                  <h5 class="card-title d-flex justify-content-between fs-3 wf-sawarabimincho">
+                    <span><?php the_title(); ?></span>
+                    <span><?php echo "¥" . number_format((int) get_field('price')); ?></span>
+                  </h5>
+                  <p class="card-text">
+                    <?php the_field('note'); ?>
+                  </p>
+                </div>
+              </div>
+            </a>
           </div>
-        </a>
-      </div>
-      <div class="col">
-        <a href="" class="text-decoration-none text-dark">
-          <div class="card h-100">
-            <img src="https://unsplash.it/300/200?random" 　class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </a>
-      </div>
-      <div class="col">
-        <a href="" class="text-decoration-none text-dark">
-          <div class="card h-100">
-            <img src="https://unsplash.it/300/200?random" 　class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </a>
-      </div>
-      <div class="col">
-        <a href="" class="text-decoration-none text-dark">
-          <div class="card h-100">
-            <img src="https://unsplash.it/300/200?random" 　class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </a>
-      </div>
+        <?php endwhile; ?>
+      <?php wp_reset_postdata();
+      endif; ?>
     </div>
   </div>
 </main><!-- #main -->
