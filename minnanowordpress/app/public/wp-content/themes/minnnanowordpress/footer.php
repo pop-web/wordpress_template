d5b68fad64464689ddf9ad8ff5232d7a278a0da1
koga yusuke <popweb782@gmail.com>
2021-10-23 00:33:59 +0900

    no message

--- minnanowordpress/app/public/wp-content/themes/minnnanowordpress/footer.php
+++ minnanowordpress/app/public/wp-content/themes/minnnanowordpress/footer.php
@@ -1,37 +1,6 @@
-<?php
-
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package minnnanowordpress
- */
-
-?>
-
-
-<footer id="colophon" class="site-footer">
-  <div class="site-info">
-    <a href="<?php echo esc_url(__('https://wordpress.org/', 'minnnanowordpress')); ?>">
-      <?php
-      /* translators: %s: CMS name, i.e. WordPress. */
-      printf(esc_html__('Proudly powered by %s', 'minnnanowordpress'), 'WordPress');
-      ?>
-    </a>
-    <span class="sep"> | </span>
-    <?php
-    /* translators: 1: Theme name, 2: Theme author. */
-    printf(esc_html__('Theme: %1$s by %2$s.', 'minnnanowordpress'), 'minnnanowordpress', '<a href="https://minnanowordpress.com/">POP</a>');
-    ?>
-  </div><!-- .site-info -->
-</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<footer>
+</footer>
 <?php wp_footer(); ?>
-
 </body>
 
 </html>
