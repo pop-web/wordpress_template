372e056b9154a4873ef1b468c5516b136984e786
koga yusuke <popweb782@gmail.com>
2022-01-06 22:23:18 +0900

    fix: 404

--- minnanowordpress/app/public/wp-content/themes/minnnanowordpress/404.php
+++ minnanowordpress/app/public/wp-content/themes/minnnanowordpress/404.php
@@ -1,3 +1,24 @@
-<?php get_header(); ?>
-記事が見つかりません。
-<?php get_footer(); ?>
+<?php
+
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ *
+ * @package minnanowordpress
+ */
+
+get_header();
+
+?>
+<main class="container my-5">
+  <div class="row">
+    <div class="col-md-8">
+      記事が見つかりません
+    </div>
+    <div class="col-md-4">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</main>
+<?php
+get_footer();
