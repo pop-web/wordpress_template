c55b4dde4b5e6fbbc4b5c1b2605a13d18d6e154b
koga yusuke <popweb782@gmail.com>
2022-02-04 23:19:34 +0900

    update functions.php

--- dental/app/public/wp-content/themes/sophie/functions.php
+++ dental/app/public/wp-content/themes/sophie/functions.php
@@ -3,10 +3,17 @@
 // global $wp_rewrite;
 // $wp_rewrite->flush_rules();
 
+// update_option('fresh_site', 1);
+// echo get_option('fresh_site');
+// exit;
 /**
- * minnanowordpress functions and definitions
+ * sophie functions and definitions
  *
- * @package minnanowordpress
+ * @link https://developer.wordpress.org/themes/
+ *
+ * basics/theme-functions/
+ *
+ * @package sophie
  */
 
 if (!defined('VERSION')) {
@@ -14,13 +21,89 @@ if (!defined('VERSION')) {
   define('VERSION', '1.0.0');
 }
 
+// テーマのデフォルト設定とWordPressの各種機能への対応登録
+if (!function_exists('setup')) :
+  function setup()
+  {
+    // headにデフォルトの投稿とコメントのRSSフィードのリンクを追加
+    add_theme_support('automatic-feed-links');
+
+    // title タグの出力
+    add_theme_support('title-tag');
+
+
+    /*
+		 * 投稿とページで投稿サムネイルをサポート
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+    add_theme_support('post-thumbnails');
+
+    // メニュー設定
+    register_nav_menus(
+      array(
+        'headernav' => 'ヘッダーナビ',
+        'footernav' => 'フッターナビ',
+      )
+    );
+
+    /*
+		 * 検索フォーム、コメントフォーム、コメントのデフォルトコアマークアップの切り替え
+		 * 有効な HTML5 を出力するように
+		 */
+    add_theme_support(
+      'html5',
+      array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+        'style',
+        'script',
+      )
+    );
+
+    // カスタム背景機能を設定
+    add_theme_support(
+      'custom-background',
+      apply_filters(
+        'custom_background_args',
+        array(
+          'default-color' => 'ffffff',
+          'default-image' => '',
+        )
+      )
+    );
+
+    // ウィジェットの選択的更新をサポートするテーマの追加
+    add_theme_support('customize-selective-refresh-widgets');
+
+    /**
+     * カスタムロゴのサポート追加
+     *
+     * @link https://codex.wordpress.org/Theme_Logo
+     */
+    add_theme_support(
+      'custom-logo',
+      array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-width'  => true,
+        'flex-height' => true,
+      )
+    );
+  }
+endif;
+add_action('after_setup_theme', 'setup');
+
 function widgets_init()
 {
   register_sidebar(
     array(
-      'name'          => esc_html__('Sidebar', 'minnanowordpress'),
+      'name'          => esc_html__('Sidebar', 'sophie'),
       'id'            => 'sidebar-1',
-      'description'   => esc_html__('Add widgets here.', 'minnanowordpress'),
+      'description'   => esc_html__('Add widgets here.', 'sophie'),
       'before_widget' => '<section id="%1$s" class="widget %2$s">',
       'after_widget'  => '</section>',
       'before_title'  => '<h2 class="widget-title">',
@@ -29,7 +112,6 @@ function widgets_init()
   );
 }
 add_action('widgets_init', 'widgets_init');
-
 
 // ウィジェットブロックエディターの停止
 function my_remove_widgets_block_editor()
@@ -52,16 +134,9 @@ function new_excerpt_more($more)
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
-// title タグの出力
-add_theme_support('title-tag');
 
-// メニュー
-register_nav_menus(
-  array(
-    'headernav' => 'ヘッダーナビ',
-    'footernav' => 'フッターナビ',
-  )
-);
+
+
 
 /**
  * Load theme style
@@ -111,3 +186,47 @@ require get_template_directory() . '/inc/customizer.php';
 if (defined('JETPACK__VERSION')) {
   require get_template_directory() . '/inc/jetpack.php';
 }
+
+
+
+// $starter_content = [
+//   'posts' => [
+//     'custom' => [
+//       'post_type' => 'post',
+//       'post_title' => 'Hello, Dolly',
+//       'post_excerpt' => 'This is a example Starter Contnet Blog post.',
+//       'post_name' => 'example-blog-post',
+//       'post_content' => "Hello, Dolly
+// Well, hello, Dolly
+// It's so nice to have you back where you belong
+// You're lookin' swell, Dolly
+// I can tell, Dolly
+// You're still glowin', you're still crowin'
+// You're still goin' strong
+// We feel the room swayin'
+// While the band's playin'
+// One of your old favourite songs from way back when
+// So, take her wrap, fellas
+// Find her an empty lap, fellas
+// Dolly'll never go away again
+// Hello, Dolly
+// Well, hello, Dolly
+// It's so nice to have you back where you belong
+// You're lookin' swell, Dolly
+// I can tell, Dolly
+// You're still glowin', you're still crowin'
+// You're still goin' strong
+// We feel the room swayin'
+// While the band's playin'
+// One of your old favourite songs from way back when
+// Golly, gee, fellas
+// Find her a vacant knee, fellas
+// Dolly'll never go away
+// Dolly'll never go away
+// Dolly'll never go away again",
+//       'comment_status' => 'closed',
+//     ]
+//   ],
+// ];
+
+// add_theme_support('starter-content', $starter_content);
