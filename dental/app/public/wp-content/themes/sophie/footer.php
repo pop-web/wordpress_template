bf654270b29234c789a33102e25cda89c9d57af4
koga yusuke <popweb782@gmail.com>
2022-02-10 23:24:16 +0900

    feat: フッターコンテンツ

--- dental/app/public/wp-content/themes/sophie/footer.php
+++ dental/app/public/wp-content/themes/sophie/footer.php
@@ -11,6 +11,56 @@
 
 ?>
 
+<div id="footer-content" class="container d-flex justify-content-between">
+  <div class="schedule">
+    <table class="table-schedule">
+      <tbody>
+        <tr>
+          <th>診療時間</th>
+          <th>月</th>
+          <th>火</th>
+          <th>水</th>
+          <th>木</th>
+          <th>金</th>
+          <th>土</th>
+          <th>日・祝</th>
+        </tr>
+        <tr>
+          <td>9:30-13:00</td>
+          <td>○</td>
+          <td>○</td>
+          <td>ー</td>
+          <td>○</td>
+          <td>○</td>
+          <td>○</td>
+          <td>ー</td>
+        </tr>
+        <tr>
+          <td>14:00-18:30</td>
+          <td>○</td>
+          <td>○</td>
+          <td>ー</td>
+          <td>○</td>
+          <td>○</td>
+          <td>△</td>
+          <td>ー</td>
+        </tr>
+      </tbody>
+    </table>
+    <p>※土曜日の午後診療は〜17:30となります。</p>
+  </div>
+  <div class="shop-info text-end d-flex flex-column justify-content-between">
+    <div class="fs-2">
+      <?php bloginfo('name'); ?>
+    </div>
+    <div class="address">
+      <div>〒123-45678</div>
+      <div>東京都渋谷区代々木1-23-45</div>
+      <div>TEL 03-123-4567 / FAX 03-123-4567</div>
+    </div>
+  </div>
+</div>
+
 <nav class="navbar navbar-expand-md px-0 py-4">
   <div class="container">
     <div class="collapse navbar-collapse">
@@ -26,16 +76,32 @@
       );
       ?>
     </div>
-    <ul class="navbar-nav">
-      <li><a href=""><i class="bi bi-line"></i></a></li>
-      <li><a href="">Twitter</a></li>
-      <li><a href="">Instagram</a></li>
-      <li><a href="">Facebook</a></li>
+    <ul id="sns-nav" class="navbar-nav">
+      <li>
+        <a href="">
+          <i class="bi bi-line"></i>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-facebook"></i>
+        </a>
+      </li>
     </ul>
   </div>
 </nav>
 
-<footer id="footer" class="site-footer py-5 mt-5 text-center">
+<footer id="footer" class="site-footer py-5 text-center">
   <div class="site-info">
     &copy; <a href="<?php echo esc_url(home_url()); ?>" class="text-decoration-none">みんなのクリニック</a> All rights reserved.
   </div>
--- dental/app/public/wp-content/themes/sophie/functions.php
+++ dental/app/public/wp-content/themes/sophie/functions.php
@@ -145,7 +145,7 @@ function theme_style()
   wp_enqueue_style('theme-style', get_stylesheet_uri(), array(), VERSION);
   wp_enqueue_script('theme-script', get_theme_file_uri('js/dist/bundle.js'),  array(), VERSION);
   // bootstrap-icons.css
-  wp_enqueue_style('bootstrap-icons', "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css", array(), VERSION);
+  wp_enqueue_style('bootstrap-icons', "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css", array(), VERSION);
 }
 add_action('wp_enqueue_scripts', 'theme_style');
 
